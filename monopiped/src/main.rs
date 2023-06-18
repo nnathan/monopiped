4a87e333dd486a0f2c9e6b2e7df50ff34aeef48a
Naveen Nathan <naveen@lastninja.net>
2023-06-17 21:21:12 -0700

    convert diagnostics into log statements

--- monopiped/src/main.rs
+++ monopiped/src/main.rs
@@ -1,19 +1,32 @@
 use std::io::{ErrorKind, Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::os::unix::io::AsRawFd;
+use std::process;
+
+use tracing::{info, error, debug};
 
 fn main() {
-    let listener = TcpListener::bind("0.0.0.0:31337").expect("Failed to bind to port 31337");
-    println!("Server listening on port 31337");
+    tracing_subscriber::fmt::init();
+
+    let listener = match TcpListener::bind("0.0.0.0:31337") {
+        Ok(listener) => listener,
+        Err(e) => {
+            error!("Failed to bind to port 31337: {:?}", e);
+            process::exit(1);
+        }
+    };
+
+    info!("Server listening on port 31337");
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                println!("New connection: {:?}", stream.peer_addr().unwrap());
+                info!("New connection: {:?}", stream.peer_addr().unwrap());
+
                 proxy_connection(stream);
             }
             Err(e) => {
-                eprintln!("Error accepting connection: {}", e);
+                error!("Error accepting connection: {}", e);
             }
         }
     }
@@ -23,22 +36,33 @@ const BACKEND_HOST: &str = "localhost";
 const BACKEND_PORT: u16 = 22;
 
 fn proxy_connection(client_stream: TcpStream) {
-    client_stream
-        .set_nonblocking(true)
-        .expect("set_nonblocking call on client_stream failed");
+    match client_stream.set_nonblocking(true) {
+        Ok(_) => {},
+        Err(e) => {
+            error!("Error setting client connection to non-blocking (not proceeding): {:?}", e);
+            return;
+        }
+    }
+
     let backend_stream = match TcpStream::connect((BACKEND_HOST, BACKEND_PORT)) {
         Ok(backend_stream) => {
-            println!("Connected to backend: {}:{}", BACKEND_HOST, BACKEND_PORT);
-            backend_stream
-                .set_nonblocking(true)
-                .expect("set_nonblocking call on backend_stream failed");
+            info!("Connected to backend: {}:{}", BACKEND_HOST, BACKEND_PORT);
             backend_stream
         }
         Err(e) => {
-            eprintln!("Failed to connect to backend: {}", e);
+            error!("Failed to connect to backend (not proceeding): {}", e);
             return;
         }
     };
+
+    match backend_stream.set_nonblocking(true) {
+        Ok(_) => {},
+        Err(e) => {
+            error!("Error setting backend connection to non-blocking (not proceeding): {:?}", e);
+            return;
+        }
+    }
+
 
     let mut sources = popol::Sources::with_capacity(2);
     let mut events = Vec::with_capacity(2);
@@ -46,92 +70,95 @@ fn proxy_connection(client_stream: TcpStream) {
     sources.register((), &client_stream, popol::interest::READ);
     sources.register((), &backend_stream, popol::interest::READ);
 
-    'outer: loop {
-        println!("entering poll");
+    loop {
+        debug!("before poll");
 
         events.clear();
 
         match sources.poll(&mut events, popol::Timeout::from_secs(1)) {
             Ok(_) => {}
             Err(e) if e.kind() == ErrorKind::TimedOut => {
-                eprintln!("poll 1 sec timeout");
+                debug!("poll timeout");
                 continue;
             }
             Err(e) => {
-                eprintln!("poll error: {:?}", e);
+                error!("poll error: {:?}", e);
                 break;
             }
         }
 
-        println!("draining {:?} events", events.len());
+        debug!("draining {:?} events", events.len());
 
         for event in events.drain(..) {
-            println!("received event {:?}", event);
+            debug!("received event {:?}", event);
 
             if event.is_invalid() {
                 /* fd was probably not opened first */
-                eprintln!(
-                    "received invalid event - shutting down socketpair: {:?}",
+                error!(
+                    "received invalid event (not proceeding): {:?}",
                     event
                 );
-                break 'outer;
+                return;
             }
 
             if event.is_error() {
                 eprintln!(
-                    "received error in event - shutting down socketpair: {:?}",
+                    "received error in event (not proceeding): {:?}",
                     event
                 );
-                break 'outer;
+                return;
             }
 
             if event.is_hangup() {
                 eprintln!(
-                    "received hangup in event - shutting down socketpair: {:?}",
+                    "received hangup in event (not proceeding): {:?}",
                     event
                 );
-                break 'outer;
+                return;
             }
 
             if event.is_readable() {
                 let source: &TcpStream;
                 let sink: &TcpStream;
+                let kind: &str;
 
                 if event.as_raw_fd() == backend_stream.as_raw_fd() {
-                    println!("source is backend_stream");
+                    debug!("source is backend, sink is client");
                     source = &backend_stream;
                     sink = &client_stream;
+                    kind = "backend";
                 } else {
+                    debug!("source is client, sink is backend");
                     source = &client_stream;
                     sink = &backend_stream;
+                    kind = "client";
                 }
 
                 match shovel(source, sink) {
                     Ok(eof) => {
+                        info!("{} connection closed", kind);
                         if eof {
-                            break 'outer;
+                            return;
                         }
                     }
                     Err(e) => {
-                        eprintln!("Error shovelling data: {:?}", e);
-                        break 'outer;
+                        error!("Error shovelling data: {:?}", e);
+                        return;
                     }
                 }
             }
         }
     }
-
-    println!("Connection closed");
 }
 
 fn shovel(mut source: &TcpStream, mut sink: &TcpStream) -> Result<bool, std::io::Error> {
     let mut buffer = [0; 4096];
 
     loop {
-        println!("read loop");
+        debug!("before read");
         let n = match source.read(&mut buffer) {
             Ok(n) => {
-                println!("read {:?} bytes", n);
+                debug!("read {:?} bytes", n);
                 n
             }
             Err(e) if e.kind() == ErrorKind::WouldBlock => return Ok(false),
@@ -148,6 +175,6 @@ fn shovel(mut source: &TcpStream, mut sink: &TcpStream) -> Result<bool, std::io:
             Err(e) => return Err(e),
         };
 
-        println!("wrote {:?} bytes", n);
+        debug!("wrote {:?} bytes", n);
     }
 }
