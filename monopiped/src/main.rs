82bb2e484ba665a68a7ebdecf2c28388f8a84050
Naveen Nathan <naveen@lastninja.net>
2023-07-04 12:25:16 -0700

    draw the rest of the owl - full communication protocol implemented

--- monopiped/src/main.rs
+++ monopiped/src/main.rs
@@ -9,13 +9,17 @@ use tracing::{debug, error, info, warn};
 
 use clap::{ArgGroup, Parser};
 
-use dryoc::classic::crypto_kx::*;
-use dryoc::classic::crypto_secretbox::{crypto_secretbox_easy, crypto_secretbox_open_easy};
-use dryoc::constants::CRYPTO_SECRETBOX_MACBYTES;
 use dryoc::classic::crypto_kdf::*;
+use dryoc::classic::crypto_kx::*;
+use dryoc::classic::crypto_secretbox::*;
+use dryoc::constants::CRYPTO_SECRETBOX_MACBYTES;
 
 use crate::utils::crypto_hash_file;
 mod utils;
+
+const CIPHERTEXT_FRAME_BYTES: usize = 1060;
+const PLAINTEXT_BYTES: usize = CIPHERTEXT_FRAME_BYTES - CRYPTO_SECRETBOX_MACBYTES;
+const PLAINTEXT_OVERHEAD: usize = 4;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -77,10 +81,8 @@ fn main() {
         let context: [u8; 8] = [0; 8];
         let mut client_key: [u8; 32] = [0; 32];
         let mut server_key: [u8; 32] = [0; 32];
-        crypto_kdf_derive_from_key(&mut client_key, 0, &context, k)
-            .expect("client key kdf failed");
-        crypto_kdf_derive_from_key(&mut server_key, 1, &context, k)
-            .expect("server key kdf failed");
+        crypto_kdf_derive_from_key(&mut client_key, 0, &context, k).expect("client key kdf failed");
+        crypto_kdf_derive_from_key(&mut server_key, 1, &context, k).expect("server key kdf failed");
         (client_key, server_key)
     };
 
@@ -134,7 +136,7 @@ const ENCRYPTED_HANDSHAKE_BYTES: usize = 32 + CRYPTO_SECRETBOX_MACBYTES;
 fn proxy_connection(
     client_stream: TcpStream,
     target: &str,
-    client: bool,
+    is_client: bool,
     client_key: &[u8; 32],
     server_key: &[u8; 32],
 ) {
@@ -170,15 +172,21 @@ fn proxy_connection(
         return;
     }
 
-    let mut tx_key = [0u8; 32];
-    let mut rx_key = [0u8; 32];
+    let mut conn_ctx = ConnectionContext {
+        tx_key: [0u8; 32],
+        rx_key: [0u8; 32],
+        rx_nonce: [0u8; 24],
+        tx_nonce: [0u8; 24],
+        rx_buf: [0u8; CIPHERTEXT_FRAME_BYTES],
+        rx_buf_len: 0,
+    };
 
-    tx_key[..].clone_from_slice(server_key);
-    rx_key[..].clone_from_slice(client_key);
+    conn_ctx.tx_key[..].clone_from_slice(server_key);
+    conn_ctx.rx_key[..].clone_from_slice(client_key);
 
-    if client {
-        tx_key[..].clone_from_slice(client_key);
-        rx_key[..].clone_from_slice(server_key);
+    if is_client {
+        conn_ctx.tx_key[..].clone_from_slice(client_key);
+        conn_ctx.rx_key[..].clone_from_slice(server_key);
     }
 
     let (pk, sk) = crypto_kx_keypair();
@@ -186,14 +194,14 @@ fn proxy_connection(
 
     let mut encrypted_pk = [0u8; ENCRYPTED_HANDSHAKE_BYTES];
 
-    if crypto_secretbox_easy(&mut encrypted_pk, &pk, &nonce, &tx_key).is_err() {
+    if crypto_secretbox_easy(&mut encrypted_pk, &pk, &nonce, &conn_ctx.tx_key).is_err() {
         error!("Error encrypting public key");
         return;
     }
 
     let mut handshake_stream = &client_stream;
 
-    if client {
+    if is_client {
         handshake_stream = &backend_stream;
     }
 
@@ -223,19 +231,34 @@ fn proxy_connection(
 
     let mut received_pk = [0u8; 32];
 
-    if crypto_secretbox_open_easy(&mut received_pk, &encrypted_pk, &nonce, &rx_key).is_err() {
+    if crypto_secretbox_open_easy(&mut received_pk, &encrypted_pk, &nonce, &conn_ctx.rx_key)
+        .is_err()
+    {
         error!("Failed to decrypted encrypted public key (aborting): authentication failure");
         return;
     }
 
-    if client {
-        if crypto_kx_client_session_keys(&mut rx_key, &mut tx_key, &pk, &sk, &received_pk).is_err()
+    if is_client {
+        if crypto_kx_client_session_keys(
+            &mut conn_ctx.rx_key,
+            &mut conn_ctx.tx_key,
+            &pk,
+            &sk,
+            &received_pk,
+        )
+        .is_err()
         {
             error!("Failed to perform key exchange (aborting)");
             return;
         }
-    } else if crypto_kx_server_session_keys(&mut rx_key, &mut tx_key, &pk, &sk, &received_pk)
-        .is_err()
+    } else if crypto_kx_server_session_keys(
+        &mut conn_ctx.rx_key,
+        &mut conn_ctx.tx_key,
+        &pk,
+        &sk,
+        &received_pk,
+    )
+    .is_err()
     {
         error!("Failed to perform key exchange (aborting)");
         return;
@@ -292,26 +315,33 @@ fn proxy_connection(
 
             let source: &TcpStream;
             let sink: &TcpStream;
-            let source_kind: &str;
-            let sink_kind: &str;
+            let source_is_backend: bool;
 
             if event.as_raw_fd() == backend_stream.as_raw_fd() {
                 source = &backend_stream;
                 sink = &client_stream;
-                source_kind = "backend";
-                sink_kind = "client";
+                source_is_backend = true;
+                debug!("source is backend, sink is client");
             } else {
                 source = &client_stream;
                 sink = &backend_stream;
-                source_kind = "client";
-                sink_kind = "backend";
+                source_is_backend = false;
+                debug!("source is client, sink is backend");
             }
 
-            debug!("source is {}, sink is {}", source_kind, sink_kind);
-
-            match shovel(source, sink) {
+            // backend -> client
+            //   - client decrypts from backend and sends to client
+            //   - server reads from backend and encrypts to client
+            // client -> backend
+            //   - client reaads from client and encrypts to backend
+            //   - server decrypts from client and sends to backend
+            match shovel(source, sink, source_is_backend, is_client, &mut conn_ctx) {
                 Ok(eof) => {
                     if eof {
+                        let mut source_kind = "client";
+                        if source_is_backend {
+                            source_kind = "backend";
+                        }
                         info!("{} connection closed", source_kind);
                         return;
                     }
@@ -365,12 +395,70 @@ fn receive_pubkey(
     }
 }
 
-fn shovel(mut source: &TcpStream, mut sink: &TcpStream) -> Result<bool, std::io::Error> {
-    let mut buffer = [0; 4096];
+fn increment_nonce(nonce: &mut [u8; 24], incr: u8) {
+    let mut acc: u16 = incr as u16;
 
+    let mut i = 0;
+
+    while i < nonce.len() {
+        acc = (nonce[i] as u16) + acc;
+        nonce[i] = (acc & 0xFF) as u8;
+        acc = acc >> 8;
+        i += 1;
+    }
+}
+
+struct ConnectionContext {
+    tx_key: [u8; 32],
+    rx_key: [u8; 32],
+    tx_nonce: [u8; 24],
+    rx_nonce: [u8; 24],
+    rx_buf: [u8; CIPHERTEXT_FRAME_BYTES],
+    rx_buf_len: usize,
+}
+
+impl ConnectionContext {
+    fn increment_tx_nonce(&mut self) {
+        increment_nonce(&mut self.tx_nonce, 1);
+    }
+
+    fn increment_rx_nonce(&mut self) {
+        increment_nonce(&mut self.rx_nonce, 1);
+    }
+
+    fn reset_rx_buf(&mut self) {
+        self.rx_buf[..].fill(0);
+        self.rx_buf_len = 0;
+    }
+}
+
+fn shovel(
+    source: &TcpStream,
+    sink: &TcpStream,
+    source_is_backend: bool,
+    is_client: bool,
+    conn_ctx: &mut ConnectionContext,
+) -> Result<bool, std::io::Error> {
+    match (source_is_backend, is_client) {
+        (false, false) => server_shovel_client_to_backend(source, sink, conn_ctx),
+        (false, true) => client_shovel_client_to_backend(source, sink, conn_ctx),
+        (true, false) => server_shovel_backend_to_client(source, sink, conn_ctx),
+        (true, true) => client_shovel_backend_to_client(source, sink, conn_ctx),
+    }
+}
+
+fn client_shovel_backend_to_client(
+    mut source: &TcpStream,
+    mut sink: &TcpStream,
+    conn_ctx: &mut ConnectionContext,
+) -> Result<bool, std::io::Error> {
     loop {
         debug!("before read");
-        let n = match source.read(&mut buffer) {
+
+        assert!(conn_ctx.rx_buf_len < CIPHERTEXT_FRAME_BYTES);
+
+        let n = match source.read(&mut conn_ctx.rx_buf[conn_ctx.rx_buf_len..CIPHERTEXT_FRAME_BYTES])
+        {
             Ok(n) => {
                 debug!("read {} bytes", n);
                 n
@@ -384,8 +472,169 @@ fn shovel(mut source: &TcpStream, mut sink: &TcpStream) -> Result<bool, std::io:
             return Ok(true);
         }
 
-        sink.write_all(&buffer[..n])?;
+        conn_ctx.rx_buf_len += n;
 
-        debug!("wrote {} bytes", n);
+        // try to see if there's more to read, otherwise exit on WouldBlock
+        if conn_ctx.rx_buf_len < CIPHERTEXT_FRAME_BYTES {
+            continue;
+        }
+
+        assert!(conn_ctx.rx_buf_len == CIPHERTEXT_FRAME_BYTES);
+
+        let _ = crypto_secretbox_open_easy_inplace(
+            &mut conn_ctx.rx_buf,
+            &conn_ctx.rx_nonce,
+            &conn_ctx.rx_key,
+        );
+        conn_ctx.increment_rx_nonce();
+
+        let len = u32::from_le_bytes(
+            conn_ctx.rx_buf[(PLAINTEXT_BYTES - PLAINTEXT_OVERHEAD)..PLAINTEXT_BYTES]
+                .try_into()
+                .unwrap(),
+        );
+
+        let len: usize = len as usize;
+        sink.write_all(&conn_ctx.rx_buf[..len])?;
+        debug!("wrote {} bytes", len);
+
+        // clean buffer for if we write again
+        conn_ctx.reset_rx_buf();
+    }
+}
+
+fn server_shovel_backend_to_client(
+    mut source: &TcpStream,
+    mut sink: &TcpStream,
+    conn_ctx: &mut ConnectionContext,
+) -> Result<bool, std::io::Error> {
+    let mut buffer = [0u8; CIPHERTEXT_FRAME_BYTES];
+
+    loop {
+        debug!("before read");
+        let n = match source.read(&mut buffer[..(PLAINTEXT_BYTES - PLAINTEXT_OVERHEAD)]) {
+            Ok(n) => {
+                debug!("read {} bytes", n);
+                n
+            }
+            Err(e) if e.kind() == ErrorKind::WouldBlock => return Ok(false),
+            Err(e) => return Err(e),
+        };
+
+        // when read() returns 0 it means EOF / closed connection
+        if n == 0 {
+            return Ok(true);
+        }
+
+        assert!(n <= (PLAINTEXT_BYTES - PLAINTEXT_OVERHEAD));
+
+        let len: u32 = n.try_into().unwrap();
+        buffer[(PLAINTEXT_BYTES - PLAINTEXT_OVERHEAD)..PLAINTEXT_BYTES]
+            .copy_from_slice(&len.to_le_bytes());
+
+        let _ = crypto_secretbox_easy_inplace(&mut buffer, &conn_ctx.tx_nonce, &conn_ctx.tx_key);
+        conn_ctx.increment_tx_nonce();
+
+        sink.write_all(&buffer)?;
+        debug!("wrote {} bytes", buffer.len());
+
+        // clean buffer for if we write again
+        buffer[..].fill(0);
+    }
+}
+
+fn client_shovel_client_to_backend(
+    mut source: &TcpStream,
+    mut sink: &TcpStream,
+    conn_ctx: &mut ConnectionContext,
+) -> Result<bool, std::io::Error> {
+    let mut buffer = [0u8; CIPHERTEXT_FRAME_BYTES];
+
+    loop {
+        debug!("before read");
+        let n = match source.read(&mut buffer[..(PLAINTEXT_BYTES - PLAINTEXT_OVERHEAD)]) {
+            Ok(n) => {
+                debug!("read {} bytes", n);
+                n
+            }
+            Err(e) if e.kind() == ErrorKind::WouldBlock => return Ok(false),
+            Err(e) => return Err(e),
+        };
+
+        // when read() returns 0 it means EOF / closed connection
+        if n == 0 {
+            return Ok(true);
+        }
+
+        assert!(n <= (PLAINTEXT_BYTES - PLAINTEXT_OVERHEAD));
+
+        let len: u32 = n.try_into().unwrap();
+        buffer[(PLAINTEXT_BYTES - PLAINTEXT_OVERHEAD)..PLAINTEXT_BYTES]
+            .copy_from_slice(&len.to_le_bytes());
+
+        let _ = crypto_secretbox_easy_inplace(&mut buffer, &conn_ctx.tx_nonce, &conn_ctx.tx_key);
+        conn_ctx.increment_tx_nonce();
+
+        sink.write_all(&buffer)?;
+        debug!("wrote {} bytes", buffer.len());
+
+        // clean buffer for if we write again
+        buffer[..].fill(0);
+    }
+}
+
+fn server_shovel_client_to_backend(
+    mut source: &TcpStream,
+    mut sink: &TcpStream,
+    conn_ctx: &mut ConnectionContext,
+) -> Result<bool, std::io::Error> {
+    loop {
+        debug!("before read");
+
+        assert!(conn_ctx.rx_buf_len < CIPHERTEXT_FRAME_BYTES);
+
+        let n = match source.read(&mut conn_ctx.rx_buf[conn_ctx.rx_buf_len..CIPHERTEXT_FRAME_BYTES])
+        {
+            Ok(n) => {
+                debug!("read {} bytes", n);
+                n
+            }
+            Err(e) if e.kind() == ErrorKind::WouldBlock => return Ok(false),
+            Err(e) => return Err(e),
+        };
+
+        // when read() returns 0 it means EOF / closed connection
+        if n == 0 {
+            return Ok(true);
+        }
+
+        conn_ctx.rx_buf_len += n;
+
+        // try to see if there's more to read, otherwise exit on WouldBlock
+        if conn_ctx.rx_buf_len < CIPHERTEXT_FRAME_BYTES {
+            continue;
+        }
+
+        assert!(conn_ctx.rx_buf_len == CIPHERTEXT_FRAME_BYTES);
+
+        let _ = crypto_secretbox_open_easy_inplace(
+            &mut conn_ctx.rx_buf,
+            &conn_ctx.rx_nonce,
+            &conn_ctx.rx_key,
+        );
+        conn_ctx.increment_rx_nonce();
+
+        let len = u32::from_le_bytes(
+            conn_ctx.rx_buf[(PLAINTEXT_BYTES - PLAINTEXT_OVERHEAD)..PLAINTEXT_BYTES]
+                .try_into()
+                .unwrap(),
+        );
+
+        let len: usize = len as usize;
+        sink.write_all(&conn_ctx.rx_buf[..len])?;
+        debug!("wrote {} bytes", len);
+
+        // clean buffer for if we write again
+        conn_ctx.reset_rx_buf();
     }
 }
