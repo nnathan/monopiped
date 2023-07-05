dfbedbf569c9a4f16ba6e6bab043601bf67fe161
Naveen Nathan <naveen@lastninja.net>
2023-07-05 07:47:18 -0700

    monocypherisation - remove crypto_generichash for mini_monocypher::crypto_blake2b_*

--- monopiped/mini-monocypher/src/lib.rs
+++ monopiped/mini-monocypher/src/lib.rs
@@ -21,3 +21,46 @@ pub fn crypto_blake2b_keyed(hash: &mut [u8], key: &[u8], message: &[u8]) {
         );
     }
 }
+
+type crypto_blake2b_ctx = monocypher_sys::crypto_blake2b_ctx;
+
+pub fn crypto_blake2b_ctx_new() -> crypto_blake2b_ctx {
+    let ctx = crypto_blake2b_ctx {
+        hash: [0u64; 8],
+        input_offset: [0u64; 2],
+        input: [0u64; 16],
+        input_idx: 0,
+        hash_size: 0,
+    };
+
+    ctx
+}
+
+pub fn crypto_blake2b_init(ctx: &mut crypto_blake2b_ctx, hash_size: usize) {
+    assert!(hash_size > 0 && hash_size <= 64);
+
+    let ctx_ptr = ctx as *mut crypto_blake2b_ctx;
+
+    unsafe {
+        monocypher_sys::crypto_blake2b_init(ctx_ptr, hash_size);
+    }
+}
+
+pub fn crypto_blake2b_update(ctx: &mut crypto_blake2b_ctx, message: &[u8]) {
+    let ctx_ptr = ctx as *mut crypto_blake2b_ctx;
+    let message_buf = message.as_ptr();
+
+    unsafe {
+        monocypher_sys::crypto_blake2b_update(ctx_ptr, message_buf, message.len());
+    }
+}
+
+pub fn crypto_blake2b_final(ctx: &mut crypto_blake2b_ctx, hash: &mut [u8]) {
+    assert!(hash.len() <= 64);
+    let ctx_ptr = ctx as *mut crypto_blake2b_ctx;
+    let hash_buf = hash.as_mut_ptr();
+
+    unsafe {
+        monocypher_sys::crypto_blake2b_final(ctx_ptr, hash_buf);
+    }
+}
--- monopiped/src/utils.rs
+++ monopiped/src/utils.rs
@@ -4,24 +4,28 @@ use std::io::Read;
 use std::path::PathBuf;
 
 use dryoc::constants::CRYPTO_SECRETBOX_NONCEBYTES;
-use dryoc::classic::crypto_generichash::*;
+use mini_monocypher::{
+    crypto_blake2b_ctx_new, crypto_blake2b_final, crypto_blake2b_init, crypto_blake2b_update,
+};
 
 pub fn crypto_hash_file(pathname: &PathBuf) -> Result<[u8; 32], std::io::Error> {
     const CHUNK_SIZE: usize = 4096;
     let mut buf = [0; CHUNK_SIZE];
     let mut f = File::open(pathname)?;
-    let mut state = crypto_generichash_init(None, 32).expect("create generichash state");
+    let mut ctx = crypto_blake2b_ctx_new();
     let mut output: [u8; 32] = [0; 32];
+
+    crypto_blake2b_init(&mut ctx, 32);
 
     loop {
         let n = f.read(&mut buf)?;
         if n < CHUNK_SIZE {
-            crypto_generichash_update(&mut state, &buf[..n]);
-            crypto_generichash_final(state, &mut output).expect("final generichash failed");
+            crypto_blake2b_update(&mut ctx, &buf[..n]);
+            crypto_blake2b_final(&mut ctx, &mut output);
             return Ok(output);
         }
 
-        crypto_generichash_update(&mut state, &buf[..n]);
+        crypto_blake2b_update(&mut ctx, &buf[..n]);
     }
 }
 
